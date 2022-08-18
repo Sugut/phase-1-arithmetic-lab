02ea6bb20527886321c1b3cede1027879c277469
Sugut <sugutvictorine@gmail.com>
2022-08-18 12:02:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
 // Write your code here
+let num1=0;
+let num2=0
+const  multiply=function(num1=31,num2=2){
+    return ( num1 * num2)
+}
+();
+
+let random= Math.floor(Math.random()*10) + 1;
+console.log (random);
+
+
+const max= Math.max(3,12,9,20);
+console.log(max);
+
+
+let num3=0, num4=0;
+const mod = function ( num3=29, num4=5){
+    return (num3 % num4)
+}
+()
+
+
+
+    
